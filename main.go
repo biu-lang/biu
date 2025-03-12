68933bc1966f92e750ca29202c92c1dcf1c8220d
Niz <mn@furzoom.com>
2025-03-12 23:58:59 +0800

    Impl binary sub operation
    Signed-off-by: Niz <mn@furzoom.com>

--- main.go
+++ main.go
@@ -18,12 +18,17 @@ func emitExpr(expr ast.Expr) {
 		fmt.Printf("# end %T\n", e)
 	case *ast.BinaryExpr:
 		fmt.Printf("# start %T\n", e)
-		emitExpr(e.X)
-		emitExpr(e.Y)
+		emitExpr(e.X) // lhs
+		emitExpr(e.Y) // rhs
 		if e.Op.String() == "+" {
-			fmt.Printf("  popq %%rax\n")
-			fmt.Printf("  popq %%rdi\n")
+			fmt.Printf("  popq %%rdi # rhs\n")
+			fmt.Printf("  popq %%rax # lhs\n")
 			fmt.Printf("  addq %%rdi, %%rax\n")
+			fmt.Printf("  pushq %%rax\n")
+		} else if e.Op.String() == "-" {
+			fmt.Printf("  popq %%rdi # rhs\n")
+			fmt.Printf("  popq %%rax # lsh\n")
+			fmt.Printf("  subq %%rdi, %%rax\n")
 			fmt.Printf("  pushq %%rax\n")
 		} else {
 			panic(fmt.Sprintf("Unexpected binary operator %s", e.Op))
@@ -36,7 +41,7 @@ func emitExpr(expr ast.Expr) {
 }
 
 func main() {
-	source := "1 + 2"
+	source := "12 - 9"
 	expr, err := parser.ParseExpr(source)
 	if err != nil {
 		panic(err)
